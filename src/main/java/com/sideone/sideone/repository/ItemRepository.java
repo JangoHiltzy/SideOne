6eb4f54600a398a5bea3f08c283e4b79287a577f
Jani Hiltunen <152394729+JangoHiltzy@users.noreply.github.com>
2025-03-19 17:05:08 +0100

    REVERTED PROJECT BACK TO EARLIER STATE
    Had trouble compiling app, reverted project back to earlier state to
    rebuild project.
    
    SIGNED-OFF-BY: Jani Hiltunen
    <152394729+JangoHiltzy@users.noreply.github.com>

--- src/main/java/com/sideone/sideone/repository/ItemRepository.java
+++
@@ -1,8 +0,0 @@
-package com.sideone.sideone.repository;
-
-import com.sideone.sideone.model.Item;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface ItemRepository extends JpaRepository<Item, Long> {
-    // You can add custom queries here if needed
-}
